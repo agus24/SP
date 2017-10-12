204ce5cdadb8d68defd80c17eaeb679419184c07
agus24 <agusx244@gmail.com>
2017-10-12 23:00:16 +0700

    commenting routes

--- app/routes.php
+++ app/routes.php
@@ -6,3 +6,4 @@ Route::get('', '\App\Controllers\BaseController@index');
 Route::get('/login', '\App\Controllers\LoginController@index');
 Route::post('/login', '\App\Controllers\LoginController@login');
 Route::get('/logout', '\App\Controllers\LoginController@logout');
+// Route::get('/user');
